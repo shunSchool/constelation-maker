5956c23008bb97669c50a827d219e083dbc82b19
shunSchool <72032942+shunSchool@users.noreply.github.com>
2022-01-10 10:32:50 -0700

    start

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ cnv.width = 1000
 cnv.height = 800
 //empyy array of stars
 let stars = [];
+
+//globL Variables
+let mouseX;
+let mouseY;
+
 //draw loop
 requestAnimationFrame(draw);
 function draw() {
@@ -12,7 +17,7 @@ function draw() {
 
     //draw stars
     for (let i = 0; i < stars.length; i ++) {
-        drawStar();
+        drawStar(stars[i]);
     }
     requestAnimationFrame(draw);
 }
@@ -20,15 +25,23 @@ function draw() {
 //star functions
 function drawStar (aStar) {
     fill ("white")
-    circle(aStar.x, aStar.y, 4)
+    circle(aStar.x, aStar.y, 4, "fill")
 }
 
-function addStar() {
-    stars.push()
-}
+
 //events
-document.addEventListener("mouseup", clickHandler);
+document.addEventListener("mousedown", clickHandler);
+document.addEventListener("mousemove", mousemoveHandler);
 
-//function clickHandler(event) {
-//    if (event)
-//}
+function clickHandler(event) {
+    stars.push({x: mouseX, y: mouseY});
+}
+
+function mousemoveHandler(event) {
+    // Get rectangle info about canvas location
+    let cnvRect = cnv.getBoundingClientRect(); 
+
+    // Calc mouse coordinates using mouse event and canvas location info
+    mouseX = Math.round(event.clientX - cnvRect.left);
+    mouseY = Math.round(event.clientY - cnvRect.top);
+}
